f98c335da4b45aecf10ad131dbba1af2ad6f182f
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-09 11:22:55 -0300

    Projeto Xadrez

---
+++ Lessons/Entities/Reservation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lessons.Entities.Exceptions;
+
+namespace Lessons.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Error in reservation: Check-Out Cannot be earlier than the Check-In");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDate(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException("Error in reservation: Dates must be in the future.");
+            }
+            else if (checkOut <= checkIn)
+            {
+                throw new DomainException("Error in reservation: Check-Out Cannot be earlier than the Check-In");
+            }
+
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber + ", check-in: " + CheckIn.ToString("dd/MM/yyyy") + ", check-out: " + CheckOut.ToString("dd/MM/yyyy") + ", " + Duration() + " nights";
+        }
+    }
+}
---
+++ Lessons/Program.cs
@@ -0,0 +1,52 @@
+﻿using Lessons.Entities;
+using Lessons.Entities.Exceptions;
+using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-In data (dd/mm/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-Out data (dd/mm/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("Update the reservation: ");
+                Console.Write("Check-In data (dd/mm/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-Out data (dd/mm/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+
+                reservation.UpdateDate(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in Reservation: " + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format Error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected Erro: " + e.Message);
+            }
+        }
+    }
+}
