1d81d94fced8908adde390702ce3b96fc1a3d05c
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-16 16:02:05 -0300

    Desafio 8

---
+++ Desafio8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desafio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CalculadoraAumento calc = new CalculadoraAumento(salario);
+
+            Console.WriteLine("Novo salario: " + calc.SalarioAumentado.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calc.Reajuste().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + calc.PercentualAumento().ToString("F0") + " %");    
+        }
+    }
+}
