255e4f839591bcad6c05ead76fcdefed7c5e896a
Breno Nogueira <breno.s.nogueira@ibm.com>
2022-08-18 15:40:29 -0300

    Desafio 10

---
+++ Desafio10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Desafio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Caro cidadão Lisarbiano, digite seu salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                double imposto = TaxCalculator.CalculateTax(salario);
+                string text = imposto > 0 ? "R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture) : "Isento";
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
---
+++ Desafio10/TaxCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio10
+{
+    static class TaxCalculator
+    {
+        private static double[] LimiteInferior = new double[] { 0, 2000.01, 3000.01, 4500.01 };
+        private static double[] LimiteSuperior = new double[] { 2000.00, 3000.00, 4500.00, 4500.01 };
+        private static double[] Taxas = new double[] { 0, 0.08, 0.18, 0.28 };
+        private static double LimiteFinal = 4500.01;
+
+        public static double CalculateTax(double salario)
+        {
+            if (salario < 0)
+                return 0;
+
+            double imposto = 0;
+            for (int i = 0; i < LimiteSuperior.Length; i++)
+            {
+                if (salario - LimiteInferior[i] < 0 && salario - LimiteSuperior[i] < 0)
+                    break;
+
+                if (LimiteSuperior[i] == LimiteFinal)
+                {   
+                    imposto += (salario - LimiteFinal) * Taxas[i];
+                    break;
+                }
+
+                if (salario - LimiteSuperior[i] < 0)
+                    imposto += (salario - LimiteInferior[i]) * Taxas[i];
+                else
+                    imposto += (LimiteSuperior[i] - LimiteInferior[i]) * Taxas[i];
+            }
+
+            return imposto;
+        }
+    }
+}
